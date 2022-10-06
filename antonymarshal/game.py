da092724ff03f8898af0b0c477ff295c7ba24737
Antony Marshal <96686984+Antonymarshal24@users.noreply.github.com>
2022-10-06 21:39:02 +0530

    hell

--- antonymarshal/game.py
+++ antonymarshal/game.py
@@ -5,5 +5,3 @@ hi everyone im antony marhsal working as software developer at zoho
 hi everyone im antony marhsal working as software developer at zoho
 
 
-hi how are you
->>>>>>> c357c5873c4f114dc7fa293e1e95d74d90b4af4b
